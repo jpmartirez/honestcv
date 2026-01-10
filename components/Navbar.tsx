f9090f43c88490786dad2179227e5c7c3388ce38
jpmartirez <johnpaulmartirez0915@gmail.com>
2026-01-10 19:23:09 +0800

    add button to the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-base-100 shadow-lg max-w-7xl mx-auto w-full flex items-center">
-        <Image src='/honestcv-logo.png' alt='HonestCV Logo' width={35} height={35}/>
-        <a className="btn btn-ghost text-xl font-bold">HonestCV</a>
+    <nav className="navbar bg-base-100 shadow-lg max-w-7xl mx-auto w-full flex justify-between">
+        <div className='flex items-center'>
+            <Image src='/honestcv-logo.png' alt='HonestCV Logo' width={35} height={35}/>
+            <a className="btn btn-ghost text-xl font-bold">HonestCV</a>
+        </div>
 
+
+        <div>
+            <button className='btn btn-info'>Get Started</button>
+        </div>
+        
     </nav>
   )
 }
